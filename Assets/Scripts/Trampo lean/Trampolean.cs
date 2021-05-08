824c6ac829ce5ee60ec96b0964b4207b1dafeb7f
Johan Malmberg <Johan.malmberg@Starstable.com>
2021-05-08 09:25:35 +0200

    trampo lean and physics material

---
+++ Assets/Scripts/Trampo lean/Trampolean.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Trampolean : MonoBehaviour
+{
+    [SerializeField] float _launchForce = 20f;
+    [SerializeField] Vector3 _direction = Vector3.up;
+    [SerializeField] Transform _directionMarker;
+
+    Vector3 _launchDirection => _direction;
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            var markerDir = _directionMarker.position - transform.position;
+            _directionMarker.position = transform.position + markerDir.normalized;
+            Debug.Log("yo moma");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Debug.DrawRay(transform.position, _launchDirection);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        other.rigidbody.velocity = Vector3.zero;
+        other.rigidbody.AddForce((_directionMarker.position - transform.position) * _launchForce, ForceMode.VelocityChange);
+    }
+
+}
