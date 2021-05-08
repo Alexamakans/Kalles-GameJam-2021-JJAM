846a0519f3c09585bbcf69103fad497863c88cb0
Alexamakans <fougner.alexander@gmail.com>
2021-05-08 13:46:10 +0200

    Added hooking functionality
    - Put an object on the Hookable layer and you will be able to hook to it using Fire1 (LMB)

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -6,15 +6,80 @@ public class Hook : MonoBehaviour
     public float range = 10f;
     public float autoTargetRadius = 3f;
 
+    [Range(0, 1)]
+    [Tooltip("In percentage of distance travelled per second")]
+    public float hookSpeed = 0.5f;
+    public float hookEndUpForce = 750f;
     public Transform raycastFrom;
-
     public LayerMask hitLayerMask;
     public LayerMask hookableLayerMask;
+    public Rigidbody body;
 
     private GameObject _target;
+    private Vector3 _targetPosition;
     private Vector3 _raycastSphereStop;
+    private bool _isHooking;
+    private float _hookPercentageDone = 0f;
+    private Vector3 _hookStartPosition;
+
+    void Reset()
+    {
+        body = GetComponentInChildren<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (_isHooking)
+        {
+            body.position = Vector3.Lerp(_hookStartPosition, _targetPosition, _hookPercentageDone);
+            _hookPercentageDone += Time.deltaTime * hookSpeed;
+
+            if (_hookPercentageDone >= 1f)
+            {
+                _hookPercentageDone = 0f;
+                _isHooking = false;
+                body.isKinematic = false;
+                body.velocity = Vector3.zero;
+                body.AddForce(Vector3.up * hookEndUpForce, ForceMode.Impulse);
+            }
+        } else if (_target)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                ShootHook();
+            }
+        }
+    }
 
     void FixedUpdate()
+    {
+        if (!_isHooking)
+        {
+            Targeting();
+        }
+    }
+
+    private void ShootHook()
+    {
+        if (!_target)
+        {
+            Debug.LogWarning($"Can't use hook without a target: {nameof(_target)} was null", this);
+            return;
+        }
+
+        if (_isHooking)
+        {
+            Debug.LogWarning($"Can't use hook while already hooking: {nameof(_isHooking)} was true", this);
+            return;
+        }
+
+        _isHooking = true;
+        body.isKinematic = true;
+        body.velocity = Vector3.zero;
+        _hookStartPosition = body.position;
+    }
+
+    private void Targeting()
     {
         var ray = new Ray(raycastFrom.position, raycastFrom.forward);
         if (Physics.Raycast(ray, out var info, range, hitLayerMask))
@@ -26,11 +91,11 @@ public class Hook : MonoBehaviour
             _raycastSphereStop = ray.origin + ray.direction * range;
         }
 
-        var playerToSphereStop = _raycastSphereStop - raycastFrom.position;
+        var playerToSphereStopDistance = (_raycastSphereStop - raycastFrom.position).magnitude;
         var hits = Physics.SphereCastAll(
             ray,
             autoTargetRadius,
-            playerToSphereStop.magnitude,
+            playerToSphereStopDistance,
             hookableLayerMask).AsEnumerable();
         hits = from hit in hits
                where IsVisibleForPlayer(hit)
@@ -39,7 +104,30 @@ public class Hook : MonoBehaviour
 
         if (hits.Count() > 0)
         {
-            _target = hits.First().collider.gameObject;
+            var hit = hits.First();
+            _target = hit.collider.gameObject;
+ 
+            if (Mathf.Approximately(Vector3.Dot(hit.normal, ray.direction), -1f))
+            {
+                // Selected collider was inside the start of the spherecast
+                // we need to do a standard raycast on it to
+                // get a proper hit.point
+                if (Physics.Raycast(
+                    raycastFrom.position,
+                    (_target.transform.position - raycastFrom.position),
+                    out var preciseInfo,
+                    range,
+                    hookableLayerMask))
+                {
+                    if (hit.collider != preciseInfo.collider)
+                    {
+                        Debug.LogWarning("Odd result, should probably investigate this", this);
+                    }
+                    hit = preciseInfo;
+                }
+            }
+            
+            _targetPosition = hit.point;
         }
         else
         {
@@ -74,7 +162,7 @@ public class Hook : MonoBehaviour
         if (_target)
         {
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(_target.transform.position, 1.5f);
+            Gizmos.DrawWireSphere(_targetPosition, 1.5f);
         }
 
         Gizmos.color = Color.red;
