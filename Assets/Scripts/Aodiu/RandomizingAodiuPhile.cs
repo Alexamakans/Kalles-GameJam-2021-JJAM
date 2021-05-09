aec16c014303249612589fbbdc0cc73b8e04c6b8
Alexamakans <fougner.alexander@gmail.com>
2021-05-09 14:48:36 +0200

    Changed GetClip to never return same twice in succession

--- Assets/Scripts/Aodiu/RandomizingAodiuPhile.cs
+++ Assets/Scripts/Aodiu/RandomizingAodiuPhile.cs
@@ -7,9 +7,19 @@ public class RandomizingAodiuPhile : ScriptableObject
 {
     [SerializeField] AudioClip[] _audioClips = new AudioClip[1];
 
+    private int _lastIndex = -1;
+
     public AudioClip GetClip()
     {
         var clipIndex = Random.Range(0, _audioClips.Length);
+
+        if (_lastIndex == clipIndex)
+        {
+            clipIndex = (clipIndex + 1) % _audioClips.Length;
+        }
+
+        _lastIndex = clipIndex;
+
         return _audioClips[clipIndex];
     }
 
